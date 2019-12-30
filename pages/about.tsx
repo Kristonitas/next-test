c97b334a6926f2582d583f72f659aa52ed728067
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2019-12-30 23:44:12 +0200

    Add about page

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import Label from "../components/label";
+
+const About = () => {
+	return (
+		<div>
+			<h1>This is the about page</h1>
+
+			<Label text="Nothing about me here" />
+		</div>
+	);
+};
+
+export default About;
